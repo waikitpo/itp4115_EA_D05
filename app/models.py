bcb8a575bdedbba8461fdff05fc549762028adcb
waikitpo <112808445+waikitpo@users.noreply.github.com>
2023-04-14 11:49:17 +0000

    company login v1

--- app/models.py
+++ app/models.py
@@ -95,33 +95,54 @@ class Post(db.Model):
 
 
 
-class Company(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    publishes = db.relationship('Job', backref='employer', lazy='dynamic')
+# class Company(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(64), index=True, unique=True)
+#     company_name = db.Column(db.String(64), index=True, unique=True)
+#     email = db.Column(db.String(120), index=True, unique=True)
+#     password_hash = db.Column(db.String(128))
+#     publishes = db.relationship('Job', backref='employer', lazy='dynamic')
+
+#     def set_password(self, password):
+#         self.password_hash = generate_password_hash(password)
+
+#     def check_password(self, password):
+#         return check_password_hash(self.password_hash, password)
+
+#     def get_reset_password_token(self, expires_in=600):
+#         return jwt.encode({"reset_password": self.id,
+#                            "exp": datetime.now(tz=timezone.utc) + timedelta(seconds=expires_in)},
+#                           app.config["SECRET_KEY"], algorithm="HS256")
+
+#     @staticmethod
+#     def verify_reset_password_token(token):
+#         try:
+#             id = jwt.decode(token, app.config["SECRET_KEY"], algorithms="HS256")[
+#                 "reset_password"]
+#         except:           
+#             return None
+#         return User.query.get(id)
 
 
-class Location(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    location = db.Column(db.String(50))
+# class Location(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     location = db.Column(db.String(50))
 
-class JobCategories(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    category = db.Column(db.String(50))
+# class JobCategories(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     category = db.Column(db.String(50))
 
-class Job(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-    location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+# class Job(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
+#     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+#     location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
 
-    title = db.Column(db.String(50), index=True, nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    requirement = db.Column(db.Text, nullable=False)
-    salary = db.Column(db.Integer, nullable=True)
+#     title = db.Column(db.String(50), index=True, nullable=False)
+#     description = db.Column(db.Text, nullable=False)
+#     requirement = db.Column(db.Text, nullable=False)
+#     salary = db.Column(db.Integer, nullable=True)
 
-    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    available = db.Column(db.Boolean)
+#     created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+#     updated_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+#     available = db.Column(db.Boolean)
--- app/routes.py
+++ app/routes.py
@@ -184,3 +184,38 @@ def unfollow(username):
     db.session.commit()
     flash(f"You are not following {username}!")
     return redirect(url_for('user', username=username))
+
+
+@app.route("/company_login", methods=['GET', 'POST'])
+def company_login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password!')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for('index')
+        redirect(next_page)
+    return render_template('company_login.html.j2', title="Company Sign In", form=form)
+
+@app.route("/company_register", methods=['GET', 'POST'])
+def company_register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congraduations, you are now a registered Company!')
+        return redirect(url_for('company_login'))
+    return render_template('company_register.html.j2', title="Register for Employer", form=form)
